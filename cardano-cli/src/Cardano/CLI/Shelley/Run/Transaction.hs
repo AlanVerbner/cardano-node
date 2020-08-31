8783cb13707622af97458555ccd464479d6d9d18
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-08-31 16:29:26 -0400

    Expand upon and refactor Cardano.CLI.Shelley.Key

--- cardano-cli/src/Cardano/CLI/Shelley/Run/Transaction.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Run/Transaction.hs
@@ -37,7 +37,7 @@ import           Ouroboros.Consensus.Shelley.Ledger (ShelleyBlock)
 import           Ouroboros.Consensus.Shelley.Protocol.Crypto (StandardShelley)
 
 import           Cardano.CLI.Environment (EnvSocketError, readEnvSocketPath, renderEnvSocketError)
-import           Cardano.CLI.Shelley.Key (SigningKeyDecodeError (..), readSigningKeyFileAnyOf)
+import           Cardano.CLI.Shelley.Key (KeyDecodeError, readSigningKeyFileAnyOf)
 import           Cardano.CLI.Shelley.Parsers
 import           Cardano.CLI.Types
 
@@ -317,7 +317,7 @@ instance Error ScriptJsonDecodeError where
 -- | Error reading the data required to construct a key witness.
 data ReadWitnessSigningDataError
   = ReadWitnessSigningDataSigningKeyDecodeError
-      !(FileError SigningKeyDecodeError)
+      !(FileError KeyDecodeError)
   | ReadWitnessSigningDataScriptError !(FileError ScriptJsonDecodeError)
   | ReadWitnessSigningDataSigningKeyAndAddressMismatch
   -- ^ A Byron address was specified alongside a non-Byron signing key.
@@ -349,7 +349,7 @@ readWitnessSigningData (ScriptWitnessSigningData (ScriptFile fp)) = do
 readWitnessSigningData (KeyWitnessSigningData skFile mbByronAddr) = do
     res <- firstExceptT ReadWitnessSigningDataSigningKeyDecodeError
       . newExceptT
-      $ readSigningKeyFileAnyOf textEnvFileTypes bech32FileTypes skFile
+      $ readSigningKeyFileAnyOf bech32FileTypes textEnvFileTypes skFile
     case (res, mbByronAddr) of
       (AByronSigningKey _ _, Just _) -> pure res
       (AByronSigningKey _ _, Nothing) -> pure res
